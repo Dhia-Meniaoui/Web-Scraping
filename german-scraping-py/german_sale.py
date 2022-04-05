42b3b9f95fdcfc3e3c1a17f28f9702a3cbfe914c
dhia <diaminiaoui@gmail.com>
2022-04-05 02:40:48 +0100

    05/04

--- german-scraping-py/german_sale.py
+++ german-scraping-py/german_sale.py
@@ -1,5 +1,6 @@
 # packages
 from ast import If
+from dataclasses import replace
 from email import header
 import scrapy
 from scrapy.crawler import CrawlerProcess
@@ -34,7 +35,7 @@ class CommercialSales(scrapy.Spider):
         
         # loop over postcodes
         for city in self.cities:
-            for count in range(1,10):
+            for count in range(1,2):
                 next_city= self.base_url+ city + '/haeuser/kaufen?d=true&sd=DESC&sf=RELEVANCE&sp='+ str(count)
                 print('sdf')
                 print(next_city)
@@ -55,18 +56,39 @@ class CommercialSales(scrapy.Spider):
             card_link = card.css('a[class="mainSection-b22fb noProject-eaed4"]::attr(href)').get()
             yield scrapy.Request(url=card_link, headers = self.headers, meta={'city':city , 'count': count}, callback = self.parse_listing)
             print(card_link)
-        
                                          
                                          
     # parse listing
     def parse_listing (self, res):
-          
-        price = str(res.css('strong[class="ng-star-inserted"] *::text').getall()[0])[:-2]
+        list1 = res.css('sd-cell-col[class="cell__col"] *::text').getall()
+        list2 = res.css('li[class="ng-star-inserted"] *::text').getall()
+        price =  str(res.css('strong[class="ng-star-inserted"] *::text').getall()[0])[:-2]
+        price = int(price.replace(".",""))
         surface = str(res.css('span[class="has-font-300"] *::text').getall()[0])
         room = str(res.css('span[class="has-font-300"] *::text').getall()[1])
+        city = str(res.meta.get('city'))
+        address1 = str(res.css('span[class="has-font-100 is-bold flex flex-wrap"] *::text').getall()[0])+str(res.css('span[class="has-font-100 is-bold flex flex-wrap"] *::text').getall()[1])
+        state = list2[list2.index('Zustand: ') + 1]
+        basement = ("voll unterkellert" in str(list2))
+        fitted_kitchen = ("Einbauküche" in str(list2))
+        Terrasse = ("Terrasse" in str(list2))
+        equipment = ("Ausstattung" in str(list2))
+        year = int(list1[list1.index('Baujahr') + 1])
+        efficiency_class = str(list1[list1.index('Effizienzklasse') + 1])
+        Category = str(list1[list1.index('Kategorie') + 1])
         self.ws.write(self.ln,1,price) 
         self.ws.write(self.ln,2,surface) 
         self.ws.write(self.ln,3,room)
+        self.ws.write(self.ln,4,city) 
+        self.ws.write(self.ln,5,address1) 
+        self.ws.write(self.ln,6,year)
+        self.ws.write(self.ln,7,efficiency_class)
+        self.ws.write(self.ln,8,Category)
+        self.ws.write(self.ln,9,state)
+        self.ws.write(self.ln,10,basement)
+        self.ws.write(self.ln,11,fitted_kitchen)
+        self.ws.write(self.ln,12,Terrasse)
+        self.ws.write(self.ln,13,equipment)
         self.ln = self.ln +1    
         
         
